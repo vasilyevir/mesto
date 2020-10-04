8456497ec4ccb46820dc51701799329b0b274883
vasilyevir <vasilyevir@yandex.ru>
2020-10-04 14:46:44 +0300

    after review 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,39 @@
 let form = document.querySelector('.form');
+let formElement = document.querySelector('.form__list')
 let name = document.querySelector('.profile__name');
 let nameChange = document.querySelector('.form__input_value_name');
 let job = document.querySelector('.profile__job');
 let jobChange = document.querySelector('.form__input_value_job');
-let editButton = document.querySelector('.profile__btnEdit');
-let closeButton = document.querySelector('.form__btnClose');
-
+let editButton = document.querySelector('.profile__btn-edit');
+let closeButton = document.querySelector('.form__btn-close');
 
 function formToggleOpen(){
-    form.classList.toggle('form_is-opened');
     nameChange.setAttribute('value', name.textContent);
-    jobChange.setAttribute('value', job.textContent);
+	jobChange.setAttribute('value', job.textContent);
+	form.classList.toggle('form_is-opened');
 }
+
 function formToggleClose() {
 	form.classList.toggle('form_is-opened');
 }
 
-function formSubmitHandler (evt) {
-	evt.preventDefault(); 
-	name.textContent = nameChange.Value;
-	job.textContent = jobChange.Value;
+function formSubmitHandler (event) {
+	event.preventDefault(); 
+	name.textContent = nameChange.value;
+	job.textContent = jobChange.value;
 	formToggleClose();
 }
 
 
 function onClickFormBackground(event) {
-	if (event.target === event.currentTarget) {
-		formToggleClose();
+	if (event.target !== event.currentTarget) {
+		return;
+	} else {
+	formToggleClose();
 	}
 }
 
 editButton.addEventListener('click', formToggleOpen);
 closeButton.addEventListener('click', formToggleClose);
 form.addEventListener('submit', formSubmitHandler);
+form.addEventListener('click', onClickFormBackground);
